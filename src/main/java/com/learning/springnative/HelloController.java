2ecdabe1b55fe5710b80f6bf5082dfe8fe1c9df5
Christopher Friedrich <christopher.friedrich1@gmail.com>
2022-07-08 21:16:30 +0200

    feat: create basic spring application

---
+++ src/main/java/com/learning/springnative/HelloController.java
@@ -0,0 +1,17 @@
+package com.learning.springnative;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/hello")
+public class HelloController {
+
+    @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
+    public String hello() {
+        return "Hello from the server";
+    }
+
+}
