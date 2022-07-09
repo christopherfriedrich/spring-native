5e7951ce4b74767c419127f157b23f42b27240af
Christopher Friedrich <christopher.friedrich1@gmail.com>
2022-07-09 12:58:44 +0200

    test: cleanup dead test code

--- src/test/java/com/learning/springnative/HelloControllerIT.java
+++ src/test/java/com/learning/springnative/HelloControllerIT.java
@@ -5,14 +5,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-@ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
 class HelloControllerIT {
     @Autowired
